bf63ac40143c382b24063284846658a23ca36af9
Nmood Build <builds@nmood.app>
2026-08-17 21:51:23 +0000

    Add Nmood iOS and Android projects

---
+++ android/app/src/main/java/com/nmood/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.nmood.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.nmood.app',
+  appName: 'Nmood',
+  webDir: 'dist'
+};
+
+export default config;
